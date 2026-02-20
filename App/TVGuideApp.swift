7d97080115a4066a8cfa564e8c4bf6175c22da8a
Bart Swart <swartb@me.com>
2026-02-20 16:16:47 +0100

    Added TVGuideApp.swift with app structure

---
+++ App/TVGuideApp.swift
@@ -0,0 +1,12 @@
+// TVGuideApp entry point in SwiftUI
+// Provides the overall app structure.
+import SwiftUI
+
+@main
+struct TVGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
